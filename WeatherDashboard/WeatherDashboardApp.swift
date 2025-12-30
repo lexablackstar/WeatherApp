496eac6365d27602924737f694a67c0e5c0cb9e5
Alexandra Marin <alexamarin2504@gmail.com>
2025-12-30 12:31:11 +0400

    Initial Commit

---
+++ WeatherDashboard/WeatherDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDashboardApp.swift
+//  WeatherDashboard
+//
+//  Created by Alexa on 30.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
